092345932675a734886dc6aff87006b4f942586a
Jorge Vargas <jorgev259@outlook.es>
2018-12-05 15:51:09 -0600

    add message when starting

--- modules/moderation/events.js
+++ modules/moderation/events.js
@@ -27,6 +27,7 @@ module.exports = {
 
         if(m.attachments.size > 0) embed.files = m.attachments.map(function(att){return {name:att.file.name,attachment:att.proxyURL}})
         m.guild.channels.find(c => c.name === 'admin-log').send(embed)
+        m.guild.channels.find(c => c.name === 'admin').send('Killer of wonder has restarted!')
       }
     }
   }
