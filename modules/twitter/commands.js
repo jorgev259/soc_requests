e9a4d06352cb5b23bb19f6c7e4ffcdfdd7e4bcd8
Jorge Vargas <jorgev259@outlook.es>
2018-12-06 15:52:56 -0600

    twitter clear

--- modules/twitter/commands.js
+++ modules/twitter/commands.js
@@ -3,25 +3,82 @@ const { log } = require('../../utilities.js')
 
 module.exports = {
   commands: {
-    'twitteradd': {
-      desc: 'Adds a twitter account to a designated channel for automatic posting. Usage: >twitteradd [username] [channel]',
+    twitteradd: {
+      desc:
+        'Adds a twitter account to a designated channel for automatic posting. Usage: >twitteradd [username] [channel]',
       async execute (client, msg, param, db) {
-        if (!param[2]) return msg.channel.send('Usage: twitter add username channel')
+        if (!param[2]) {
+          return msg.channel.send('Usage: twitter add username channel')
+        }
         let username = param[1]
         let channel = param[2]
 
-        if (msg.mentions.channels.size > 0) channel = msg.mentions.channels.first().name
-        if (!msg.guild.channels.some(c => c.name === channel)) return msg.channel.send('Channel doesnt exist')
+        if (msg.mentions.channels.size > 0) {
+          channel = msg.mentions.channels.first().name
+        }
+        if (!msg.guild.channels.some(c => c.name === channel)) {
+          return msg.channel.send('Channel doesnt exist')
+        }
 
-        twitter.get('users/show', { screen_name: username }).then(res => {
-          stream(client, db, [res.data.id_str])
-          db.prepare('INSERT INTO twitter (id,channel) VALUES (?,?)').run(res.data.id_str, channel)
-          msg.channel.send('Account added!')
-        }).catch(err => {
-          console.log(err)
-          if (err.code === 50) return msg.channel.send('User not found')
-          log(client, err.message || err.stack)
-          msg.channel.send('Something went wrong!')
+        twitter
+          .get('users/show', { screen_name: username })
+          .then(res => {
+            stream(client, db, [res.data.id_str])
+            db.prepare('INSERT INTO twitter (id,channel) VALUES (?,?)').run(
+              res.data.id_str,
+              channel
+            )
+            msg.channel.send('Account added!')
+          })
+          .catch(err => {
+            console.log(err)
+            if (err.code === 50) return msg.channel.send('User not found')
+            log(client, err.message || err.stack)
+            msg.channel.send('Something went wrong!')
+          })
+      }
+    },
+
+    twitterclear: {
+      desc: 'Rejects all tweets waiting for approval',
+      async execute (client, msg, param, db) {
+        await msg.channel.send(
+          `${
+            msg.author
+          } Are you sure you want to reject all current tweets? Write "yes" to continue`
+        )
+        const filter = m => m.author.id === msg.author.id
+        msg.channel.awaitMessages(filter, { max: 1 }).then(async collected => {
+          try {
+            let m = collected.first()
+            if (m.content.toLowerCase() === 'yes') {
+              let purge = await m.channel.send('Starting purge')
+              let channel1 = client.channels.find(
+                c => c.name === 'tweet-approval'
+              )
+              let channel2 = await channel1.clone()
+              await channel1.delete()
+
+              db.prepare('DELETE FROM tweets').run()
+              purge.edit('Purge complete')
+
+              let position =
+                client.channels.find(c => c.name === 'tweet-approval-log')
+                  .rawPosition
+
+              await channel2.edit({
+                position: position
+              })
+              client.channels
+                .find(c => c.name === 'tweet-approval-log')
+                .send(`#tweet-approval cleared by ${msg.author}`)
+            } else {
+              msg.channel.send('Purge stopped')
+            }
+          } catch (err) {
+            console.log(err)
+            msg.channel.send('Something went wrong')
+          }
         })
       }
     }
--- modules/twitter/events.js
+++ modules/twitter/events.js
@@ -1,38 +1,58 @@
 let { stream } = require('./util.js')
-let reactions = ['✅', '❎','❓']
+let reactions = ['✅', '❎', '❓']
 
 module.exports = {
   async reqs (client, db) {
     return new Promise((resolve, reject) => {
-      db.prepare('CREATE TABLE IF NOT EXISTS twitter (id TEXT, channel TEXT, PRIMARY KEY (id,channel))').run()
-      db.prepare('CREATE TABLE IF NOT EXISTS tweets (id TEXT, url TEXT, channel TEXT, PRIMARY KEY (id))').run()
+      db.prepare(
+        'CREATE TABLE IF NOT EXISTS twitter (id TEXT, channel TEXT, PRIMARY KEY (id,channel))'
+      ).run()
+      db.prepare(
+        'CREATE TABLE IF NOT EXISTS tweets (id TEXT, url TEXT, channel TEXT, PRIMARY KEY (id))'
+      ).run()
       resolve()
     })
   },
   events: {
     async ready (client, db) {
-      let ids = db.prepare('SELECT id FROM twitter').all().map(r => r.id)
+      let ids = db
+        .prepare('SELECT id FROM twitter')
+        .all()
+        .map(r => r.id)
 
-      await client.channels.find(c => c.name === 'tweet-approval').messages.fetch()
+      await client.channels
+        .find(c => c.name === 'tweet-approval')
+        .messages.fetch()
       if (ids.length > 0) stream(client, db, ids)
     },
 
     async messageReactionAdd (client, db, reaction, user) {
-      if (reaction.message.channel.name === 'tweet-approval' && !user.bot && reactions.includes(reaction.emoji.name)) {
+      if (
+        reaction.message.channel.name === 'tweet-approval' &&
+        !user.bot &&
+        reactions.includes(reaction.emoji.name)
+      ) {
         let embed = reaction.message.embeds[0]
 
         switch (reaction.emoji.name) {
           case '✅':
             embed.setFooter(`Accepted by ${user}`)
             let url = ''
-            let msgs = db.prepare('SELECT channel,url FROM tweets WHERE id=?').all(reaction.message.id).map(row => {
-              url = row.url
-              return client.channels.find(c => c.name === row.channel).send(row.url)
-            })
+            let msgs = db
+              .prepare('SELECT channel,url FROM tweets WHERE id=?')
+              .all(reaction.message.id)
+              .map(row => {
+                url = row.url
+                return client.channels
+                  .find(c => c.name === row.channel)
+                  .send(row.url)
+              })
 
             Promise.all(msgs).catch(err => {
               console.log(err)
-              client.channels.find(c => c.name === 'tweet-approval-log').send(`A message couldnt be send in some channels. URL: ${url}`)
+              client.channels
+                .find(c => c.name === 'tweet-approval-log')
+                .send(`A message couldnt be send in some channels. URL: ${url}`)
             })
 
             sendLog(client, db, reaction, embed, 'tweet-approval-log')
@@ -44,23 +64,36 @@ module.exports = {
             break
 
           case '❓':
-            let question = await reaction.message.channel.send(`${user} type (or mention) the name of the channel where you want to send the tweet.`)
-            const filter = m => m.mentions.channels.size > 0 || reaction.message.guild.channels.some(c => c.name === m.content)
-            reaction.message.channel.awaitMessages(filter, { max: 1 })
+            let question = await reaction.message.channel.send(
+              `${user} type (or mention) the name of the channel where you want to send the tweet.`
+            )
+            const filter = m =>
+              m.mentions.channels.size > 0 ||
+              reaction.message.guild.channels.some(c => c.name === m.content)
+            reaction.message.channel
+              .awaitMessages(filter, { max: 1 })
               .then(collected => {
                 let channel
-                if (collected.first().mentions.channels.size > 0) channel = collected.first().mentions.channels.first()
-                else channel = reaction.message.guild.channels.find(c => c.name === collected.first().content)
+                if (collected.first().mentions.channels.size > 0) { channel = collected.first().mentions.channels.first() } else {
+                  channel = reaction.message.guild.channels.find(
+                    c => c.name === collected.first().content
+                  )
+                }
 
                 embed.setFooter(`Accepted by ${user}`)
 
-                let url = db.prepare('SELECT channel,url FROM tweets WHERE id=?').all(reaction.message.id)[0].url
+                let url = db
+                  .prepare('SELECT channel,url FROM tweets WHERE id=?')
+                  .all(reaction.message.id)[0].url
                 channel.send(url).catch(err => {
                   console.log(err)
-                  client.channels.find(c => c.name === 'tweet-approval-log').send(`A message couldnt be send in some channels. URL: ${url}`)
+                  client.channels
+                    .find(c => c.name === 'tweet-approval-log')
+                    .send(
+                      `A message couldnt be send in some channels. URL: ${url}`
+                    )
                 })
 
-                
                 sendLog(client, db, reaction, embed, 'tweet-approval-log')
                 question.delete()
                 collected.first().delete()
