8d1f4442df003cabb09be40bb06310a48e1b0378
Jorge Vargas <jorgev259@outlook.es>
2018-12-17 09:59:21 -0600

    fix path

--- modules/padoru/commands.js
+++ modules/padoru/commands.js
@@ -1,6 +1,7 @@
 const moment = require('moment')
 const chance = require('chance').Chance()
 const { MessageEmbed } = require('discord.js')
+const path = require('path')
 
 let greetings = [
   '{user} just sent his redGuards to {sent}. And they brought a <:gjallardoodle:522816784580214804>!',
@@ -93,7 +94,7 @@ module.exports = {
               `<@${sendId}> a Loremaster appears in front of you after taking a bite of the <:gjallardoodle:522816784580214804>. The loremaster says "Stay Frosty" as he hands ${ammount} <:gjallardoodle:522816784580214804> to you and ${
                 msg.author
               } and allows ${msg.author} to make another gift.`,
-              { files: ['./modules/padoru/resources/MagicJoker.jpg'] }
+              { files: [path.join(__dirname, 'modules/padoru/resources/MagicJoker.jpg')] }
             )
             db.prepare(
               'UPDATE greetingsReceived SET count=count+? WHERE id=?'
@@ -147,7 +148,7 @@ module.exports = {
           .attachFiles([
             {
               name: 'doodle.jpg',
-              attachment: './modules/padoru/resources/doodle.jpg'
+              attachment: path.join(__dirname, 'modules/padoru/resources/doodle.jpg')
             }
           ])
           .setThumbnail('attachment://doodle.jpg')
