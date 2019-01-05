cfb7801191ca21bdeeeee1d396bbe915539096a7
Jorge Vargas <jorgev259@outlook.es>
2019-01-05 17:49:02 -0600

    softban command

---
+++ modules/moderation/commands.js
@@ -0,0 +1,16 @@
+module.exports = {
+    commands: {
+      softban: {
+        desc: 'Ban an user by id',
+        usage: '>softban [id]',
+        execute (client, msg, param, db) {
+            if(!param[1]) return msg.channel.send('No id was provided')
+            msg.guild.members.ban(param[1]).then(()=>{
+                msg.channel.send('Ban succesful')
+            }).catch(()=>{
+                msg.channel.send('Something went wrong')
+            })
+        }
+      }
+    }
+  }
