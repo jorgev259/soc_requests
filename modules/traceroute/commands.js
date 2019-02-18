7f64b3e89055530f57addc72998f72ea31a67fd1
Jorge Vargas <jorgev259@outlook.es>
2019-02-18 16:22:32 -0600

    traceroute

--- modules/traceroute/commands.js
+++ modules/traceroute/commands.js
@@ -1,4 +1,6 @@
 var TraceRoute = require('http-traceroute')
+const Traceroute = require('nodejs-traceroute')
+
 module.exports = {
   commands: {
     trace: {
@@ -6,7 +8,7 @@ module.exports = {
       usage: '>trace [url or ip]',
       execute (client, msg, param, db) {
         if (!param[1]) return msg.channel.send('Please provide a url or ip')
-        var trace = new TraceRoute(param[1])
+        /* var trace = new TraceRoute(param[1])
 
         trace.on('readable', function () {
           var hop = null
@@ -17,7 +19,28 @@ module.exports = {
         })
 
         trace.once('error', function () { msg.channel.send('Something went wrong') })
-        trace.once('end', function () { msg.channel.send('Finished') })
+        trace.once('end', function () { msg.channel.send('Finished') }) */
+        
+        try {
+          const tracer = new Traceroute()
+          tracer
+            .on('pid', (pid) => {
+              msg.channel.send(`pid: ${pid}`)
+            })
+            .on('destination', (destination) => {
+              msg.channel.send(`destination: ${destination}`)
+            })
+            .on('hop', (hop) => {
+              msg.channel.send(`hop: ${JSON.stringify(hop)}`)
+            })
+            .on('close', (code) => {
+              msg.channel.send(`close: code ${code}`)
+            })
+
+          tracer.trace(param[1])
+        } catch (ex) {
+          console.log(ex)
+        }
       }
     }
   }
