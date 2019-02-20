d5dea2298f46df0f6628fc569bd1c8e8a0e59c60
Jorge Vargas <jorgev259@outlook.es>
2019-02-20 10:42:46 -0600

    rework

--- modules/traceroute/commands.js
+++ modules/traceroute/commands.js
@@ -8,16 +8,22 @@ module.exports = {
       async execute (client, msg, param, db) {
         if (!param[1]) return msg.channel.send('Please provide a url or ip')
 
+        let content = 'Starting'
+        let sent = await msg.channel.send(content, { code: true })
+
+        function info (info) {
+          content += `\n${info}`
+          sent.edit(content, { code: true })
+        }
+
         try {
           const tracer = new Traceroute()
-          let content = 'Starting'
-          let sent = await msg.channel.send(content, { code: true })
 
           tracer
-            .on('pid', pid => info(sent, content, `pid: ${pid}`))
-            .on('destination', destination => info(sent, content, `destination: ${destination}`))
-            .on('hop', hop => info(sent, content, `${hop.hop}) ${hop.hostname ? `${hop.hostname} (${hop.ip})` : hop.ip} ${hop.rtt1 ? hop.rtt1 : ''}`))
-            .on('close', code => info(sent, content, `close: code ${code}`))
+            .on('pid', pid => info(`pid: ${pid}`))
+            .on('destination', destination => info(`destination: ${destination}`))
+            .on('hop', hop => info(`${hop.hop}) ${hop.hostname ? `${hop.hostname} (${hop.ip})` : hop.ip} ${hop.rtt1 ? hop.rtt1 : ''}`))
+            .on('close', code => info(`close: code ${code}`))
 
           tracer.trace(param[1])
         } catch (ex) {
@@ -26,9 +32,4 @@ module.exports = {
       }
     }
   }
-}
-
-function info (sent, content, info) {
-  content += `\n${info}`
-  sent.edit(content, { code: true })
 }
