1d40d4c12125dcd97d1c864772c34bca904f5ba8
Jorge Vargas <jorgev259@outlook.es>
2019-03-13 13:50:52 -0600

    add server id

--- modules/speedtest/commands.js
+++ modules/speedtest/commands.js
@@ -2,9 +2,12 @@ var speedTest = require('speedtest-net')
 module.exports.commands = {
   'speedtest': {
     desc: 'Performs a speedtest.',
-    usage: '>speedtest',
+    usage: '>speedtest [server]',
     execute (client, msg, param, db) {
-      speedTest.visual({ log: false }, (err, data) => {
+      let options = { log: false }
+      if (param[1]) options.serverId = param[1]
+
+      speedTest.visual(options, (err, data) => {
         if (err) {
           msg.channel.send('Something went wrong!')
           throw err
