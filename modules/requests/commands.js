177ebc5f5dd348b10e49fcefba3ddce9e347300d
Jorge Vargas <jorgev259@outlook.es>
2019-03-14 16:20:01 -0600

    limit donators

--- modules/requests/commands.js
+++ modules/requests/commands.js
@@ -16,7 +16,7 @@ module.exports = {
         if (!param[1]) return msg.channel.send('Please provide a url or name')
 
         let req = db.prepare('SELECT request FROM requests WHERE user=?').get(msg.author.id)
-        if (req) return msg.channel.send(`The request '${req.request}' is still on place. Wait until its fulfilled or rejected.`)
+        if (!msg.member.roles.some(r => r.name === 'Donators') && req) return msg.channel.send(`The request '${req.request}' is still on place. Wait until its fulfilled or rejected.`)
 
         let name = param.slice(1).join(' ')
 
