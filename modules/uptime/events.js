d8ed01982bdf7d0ffa2e17d42e4c96745a664778
Jorge Vargas <jorgev259@outlook.es>
2019-08-05 17:23:16 -0600

    modularize

--- modules/uptime/events.js
+++ modules/uptime/events.js
@@ -3,22 +3,31 @@ let status
 module.exports.events = {
   async ready (client, db) {
     get('https://www.sittingonclouds.net/').then(res => {
+      console.log(res.status)
       status = res.status
-      setInterval(() => {
-        get('https://www.sittingonclouds.net/').then(res => {
-          console.log(res.status)
-          if (res.status !== status) {
-            status = res.status
-            client.guilds.first().channels.find(c => c.name === 'administration').send(`www.sittingonclouds.net status changed to ${status}`)
-          }
-        }).catch(res => {
-          console.log(res.status)
-          if (res.status !== status) {
-            status = res.status
-            client.guilds.first().channels.find(c => c.name === 'administration').send(`www.sittingonclouds.net status changed to ${status}`)
-          }
-        })
-      }, 1000)
+      start(client)
+    }).catch(res => {
+      console.log(res.status)
+      status = res.status
+      start(client)
     })
   }
+}
+
+function start (client) {
+  setInterval(() => {
+    get('https://www.sittingonclouds.net/').then(res => {
+      console.log(res.status)
+      if (res.status !== status) {
+        status = res.status
+        client.guilds.first().channels.find(c => c.name === 'administration').send(`www.sittingonclouds.net status changed to ${status}`)
+      }
+    }).catch(res => {
+      console.log(res.status)
+      if (res.status !== status) {
+        status = res.status
+        client.guilds.first().channels.find(c => c.name === 'administration').send(`www.sittingonclouds.net status changed to ${status}`)
+      }
+    })
+  }, 1000)
 }
