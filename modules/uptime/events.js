4b8b94c844e83ff2ffe30bf9d89e7c9e311d53c8
Jorge Vargas <jorgev259@outlook.es>
2019-08-05 17:15:01 -0600

    refactor

--- modules/uptime/events.js
+++ modules/uptime/events.js
@@ -1,17 +1,19 @@
 let { get } = require('axios')
-let status = 200
+let status
 module.exprot = {
   async ready (client, db) {
     get('https://www.sittingonclouds.net/').then(res => {
       status = res.status
+      setTimeout(() => {
+        get('https://www.sittingonclouds.net/').then(res => {
+          console.log(res.status)
+          if (res.status !== status) {
+            status = res.status
+            client.guilds.first().channels.find(c => c.name === 'administration').send(`www.sittingonclouds.net status changed to ${status}`)
+          }
+        })
+      }, 1000)
     })
-    setTimeout(() => {
-      get('https://www.sittingonclouds.net/').then(res => {
-        if (res.status !== status) {
-          status = res.status
-          client.guilds.first().channels.find(c => c.name === 'administration').send(`www.sittingonclouds.net status changed to ${status}`)
-        }
-      })
-    }, 1000)
+    
   }
 }
