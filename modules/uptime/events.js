ea038c4eeb1b5063cfd994f53c8a4c7b1d13c625
Jorge Vargas <jorgev259@outlook.es>
2019-08-06 11:49:27 -0600

    handle async

--- modules/uptime/events.js
+++ modules/uptime/events.js
@@ -14,22 +14,21 @@ module.exports.events = {
 }
 
 function start (client) {
-  setInterval(() => {
-    get('https://www.sittingonclouds.net/').then(res => {
-      handle(client, res.status)
-    }).catch(err => {
-      handle(client, err.response.status)
-    })
-  }, 5000)
+  get('https://www.sittingonclouds.net/').then(res => {
+    handle(client, res.status)
+  }).catch(err => {
+    handle(client, err.response.status)
+  })
 }
 
-function handle (client, statusInc) {
+async function handle (client, statusInc) {
   if (statusInc !== status) {
     status = statusInc
-    client.guilds.first().channels.find(c => c.name === 'server-downtime').send(
+    await client.guilds.first().channels.find(c => c.name === 'server-downtime').send(
       status === 200
-        ? `**:white_check_mark: sittingonclouds.net Server is Up: It should work fine again! - Reason: Server is Reachable`
+        ? `**:white_check_mark: sittingonclouds.net Server is Up: It should work fine again! - Reason: Server is Reachable**`
         : `**:no_entry: sittingonclouds.net Server is Down: Its Probally a Maintainance or its really Down. - Reason: Connection Timeout - Time: ${moment().utc().format('YYYY/MM/DD hh:mm:ss A')} UTC**`
     )
   }
+  setTimeout(start, 5000, client)
 }
