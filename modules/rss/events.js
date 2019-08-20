32befe3f2bd25313a09b79bc1865ed3e33230996
jorgev259 <jorgev259@outlook.es>
2019-08-20 00:07:59 -0600

    debug

--- modules/rss/events.js
+++ modules/rss/events.js
@@ -1,10 +1,13 @@
 var AsyncPolling = require('async-polling')
 const Parser = require('rss-parser')
-let parser = new Parser()
+const parser = new Parser()
 var rss = {}
+
+const urls = require('data/rss/rss.json')
 module.exports.events = {
   async ready (client, db) {
-    client.data.rss.forEach(url => {
+    console.log(urls)
+    urls.forEach(url => {
       console.log(url)
     })
   }
