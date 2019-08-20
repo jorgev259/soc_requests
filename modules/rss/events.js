ffe416741e5e98803af68dde21c2d5f325c80235
jorgev259 <jorgev259@outlook.es>
2019-08-20 00:46:29 -0600

    rss

--- modules/rss/events.js
+++ modules/rss/events.js
@@ -2,28 +2,46 @@ var asyncPolling = require('async-polling')
 const Parser = require('rss-parser')
 const parser = new Parser()
 var rss = {}
+var cache = {}
 const urls = ['https://www.hetzner-status.de/en.atom', 'https://www.cloudflarestatus.com/history.atom']
 
-module.exports.events = {
-  async ready (client, db) {
-    urls.forEach(url => {
-      rss[url] = asyncPolling(async function (end) {
-        const feed = await parser.parseURL(url)
-        console.log(feed)
-
-        end()
-      }, 1000)
-
-      rss[url].on('error', function (error) {
-        console.log(error)
-      })
-
-      rss[url].on('result', function (result) {
-        if (result) {
-          console.log(result)
+module.exports = {
+  async reqs (client, db) {
+    db.prepare('CREATE TABLE IF NOT EXISTS rss (id TEXT, url TEXT, PRIMARY KEY (id,url))').run()
+  },
+  events: {
+    async ready (client, db) {
+      urls.forEach(async url => {
+        const data = db.prepare('SELECT id FROM rss WHERE url=?').get(url)
+        if (data) cache[url] = data.id
+        else {
+          const feed = await parser.parseURL(url)
+          db.prepare('INSERT INTO rss (id,url) VALUES (?,?)').run(feed.items[0].id, url)
+          cache[url] = feed.items[0].id
         }
+        rss[url] = asyncPolling(async function (end) {
+          const feed = await parser.parseURL(url)
+          const outItems = []
+          var run = true
+          for (var i = 0; i < feed.items.length && run; i++) {
+            if (feed.items[i] !== cache[url]) outItems.push(feed.items[i])
+            else run = false
+          }
+          if (outItems.length > 0) {
+            db.prepare('UPDATE rss SET id = ?, url = ? WHERE id = ?').run(outItems[0].id, url, cache[url])
+            cache[url] = outItems[0].id
+          }
+          console.log(outItems)
+
+          end()
+        }, 1000)
+
+        rss[url].on('error', function (error) {
+          console.log(error)
+        })
+
+        rss[url].run()
       })
-      rss[url].run()
-    })
+    }
   }
 }
