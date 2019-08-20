23d0eae60b6ea0471d01a4fafc37e2faf96faff4
jorgev259 <jorgev259@outlook.es>
2019-08-20 00:21:00 -0600

    rss

--- modules/rss/events.js
+++ modules/rss/events.js
@@ -1,13 +1,29 @@
-var AsyncPolling = require('async-polling')
+var asyncPolling = require('async-polling')
 const Parser = require('rss-parser')
 const parser = new Parser()
 var rss = {}
+const urls = ['https://www.hetzner-status.de/en.atom', 'https://www.cloudflarestatus.com/history.atom']
 
 module.exports.events = {
   async ready (client, db) {
-      console.log(client.data)
-    client.data.config.rss.forEach(url => {
-      console.log(url)
+    urls.forEach(url => {
+      rss[url] = asyncPolling(async function (end) {
+        const feed = await parser.parseURL(url)
+        console.log(feed)
+
+        end()
+      }, 1000)
+
+      rss[url].on('error', function (error) {
+        console.log(error)
+      })
+
+      rss[url].on('result', function (result) {
+        if (result) {
+          console.log(result)
+        }
+      })
+      rss[url].run()
     })
   }
 }
