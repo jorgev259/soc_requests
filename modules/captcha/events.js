d7429f918da8b2c58bb62015ba7b6423035e751c
Jorge Vargas <jorgev259@outlook.es>
2019-09-02 16:58:57 -0600

    add timeout

--- modules/captcha/events.js
+++ modules/captcha/events.js
@@ -1,24 +1,46 @@
 let { generate } = require('captcha-generator')
+let cache = {}
 
 module.exports = {
   reqs (client, db) {
-    db.prepare('CREATE TABLE IF NOT EXISTS captcha (id TEXT, guild TEXT, captcha TEXT, PRIMARY KEY(id,guild))').run()
+    db.prepare('CREATE TABLE IF NOT EXISTS captcha (id TEXT, guild TEXT, captcha TEXT, attempts INTEGER, joinStamp INTEGER, PRIMARY KEY(id,guild))').run()
   },
   events: {
+    ready (client, db) {
+      let rows = db.prepare('SELECT id,guild,joinStamp FROM captcha').all()
+      rows.forEach(row => {
+        let guild = client.guilds.get(row.guild)
+        guild.members.fetch(row.id).then(member => {
+          cache[`${member.guild.id}_${member.id}`] = setTimeout(member.kick, (30 * 60 * 1000) - (Date.now() - row.joinStamp))
+        })
+      })
+    },
     message (client, db, moduleName, msg) {
       if (!msg.guild) {
-        let rows = db.prepare('SELECT guild,captcha FROM captcha WHERE id = ?').all(msg.author.id)
+        let rows = db.prepare('SELECT guild,captcha,attempts FROM captcha WHERE id = ?').all(msg.author.id)
         if (rows.length > 0) {
           rows.forEach(row => {
             if (row.captcha === msg.content) {
               let guild = client.guilds.get(row.guild)
               let role = guild.roles.find(r => r.name === 'Members')
               guild.members.fetch(msg.author.id).then(member => {
-                db.prepare('SELECT guild,captcha FROM captcha WHERE id = ? AND guild = ?').all(msg.author.id, row.guild)
+                clearTimeout(cache[`${guild.id}_${msg.author.id}`])
+                db.prepare('DELETE FROM captcha WHERE id = ? AND guild = ?').all(msg.author.id, row.guild)
                 member.roles.add(role)
               })
             } else {
-              msg.author.send('Wrong captcha')
+              if (row.attempts === 1) {
+                db.prepare('DELETE FROM captcha WHERE id = ? AND guild = ?').all(msg.author.id, row.guild)
+                let guild = client.guilds.get(row.guild)
+
+                msg.author.send('Too many failed attempts.')
+                guild.members.fetch(msg.author.id).then(member => member.kick())
+
+                clearTimeout(cache[`${guild.id}_${msg.author.id}`])
+              } else {
+                db.prepare('UPDATE captcha SET attempts = attempts - 1 WHERE id = ? AND guild = ?')
+                msg.author.send(`Wrong captcha. Attempts left: ${row.attempts - 1}`)
+              }
             }
           })
         }
@@ -30,7 +52,7 @@ module.exports = {
 
       let rows = db.prepare('SELECT guild,captcha FROM captcha WHERE id = ? AND guild = ?').get(member.id, member.guild.id)
       if (!rows) {
-        db.prepare('INSERT INTO captcha (id,guild,captcha) VALUES (?,?,?)').run(member.id, member.guild.id, plaintext)
+        db.prepare('INSERT INTO captcha (id,guild,captcha,attempts,joinStamp) VALUES (?,?,?,5,?)').run(member.id, member.guild.id, plaintext, member.joinedTimestamp)
 
         member.send({
           embed: {
@@ -43,6 +65,8 @@ module.exports = {
           },
           files: [buffer]
         })
+
+        cache[`${member.guild.id}_${member.id}`] = setTimeout(member.kick, 30 * 60 * 1000)
       }
     }
   }
