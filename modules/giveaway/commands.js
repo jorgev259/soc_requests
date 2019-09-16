540eaa5a1c088693792cd7237ef410af23866f1e
jorgev259 <jorgev259@outlook.es>
2019-09-15 23:06:24 -0600

    giveaway

---
+++ modules/giveaway/commands.js
@@ -0,0 +1,46 @@
+module.exports = {
+  reqs (client, db) {
+    db.prepare('CREATE TABLE IF NOT EXISTS giveaway (guild TEXT, channel TEXT, code TEXT, hint INTEGER)').prepare()
+  },
+  commands: {
+    giveaway: {
+      desc: 'Starts a giveaway on the current channel.',
+      usage: 'giveaway',
+      async execute (client, msg, param, db) {
+        let check = db.prepare('SELECT * FROM giveaway WHERE guild = ? AND channel = ?').get(msg.guild.id, msg.channel.id)
+        if (check) return msg.channel.send('Theres already a giveaway running on this channel')
+
+        const dm = await msg.author.send('Send the code to giveaway here.')
+        dm.channel.awaitMessages(m => true, { max: 1 })
+          .then(collected => {
+            const code = msg.first().content
+            var match = code.match(/\d+/)
+            const hint = code
+            hint[match] = '?'
+
+            check = db.prepare('SELECT * FROM giveaway WHERE guild = ? AND channel = ?').get(msg.guild.id, msg.channel.id)
+            if (check) return msg.channel.send('Theres already a giveaway running on this channel')
+
+            db.prepare('INSERT INTO giveaways (guild,channel,code,hint) VALUES (?,?,?,?)').run(msg.guild.id, msg.channel.id, code, match)
+            msg.channel.send(`Giveaway started! Use the command 'guess' to try the missing number of this code.\n${hint}`)
+          })
+      }
+    },
+    guess: {
+      desc: 'Tries a guess for a giveaway.',
+      usage: 'guess [0-9]',
+      async execute (client, msg, param, db) {
+        const giveaway = db.prepare('SELECT * FROM giveaway WHERE guild = ? AND channel = ?').get(msg.guild.id, msg.channel.id)
+        if (!giveaway) return msg.channel.send('Theres no giveaway running on this channel')
+
+        const guess = parseInt(param[1])
+        const { code, hint } = giveaway
+        if (code[hint] === guess) {
+          db.prepare('DELETE FROM giveaways WHERE guild = ? AND channel = ? AND code = ? AND hint = ?').run(msg.guild.id, msg.channel.id, code, guess)
+          msg.channel.send(`Congratulations ${msg.author}! Your code was sent throught DMs.`)
+          msg.author.send(code)
+        } else msg.channel.send('Try again.')
+      }
+    }
+  }
+}
