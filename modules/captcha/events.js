bec94b084d383c5243a04614ade64a04a08057e9
jorgev259 <jorgev259@outlook.es>
2019-10-06 23:17:23 -0500

    add reroll

--- modules/captcha/events.js
+++ modules/captcha/events.js
@@ -1,5 +1,5 @@
-let { generate } = require('captcha-generator')
-let cache = {}
+const { generate } = require('captcha-generator')
+const cache = {}
 
 module.exports = {
   reqs (client, db) {
@@ -7,9 +7,9 @@ module.exports = {
   },
   events: {
     ready (client, db) {
-      let rows = db.prepare('SELECT id,guild,joinStamp FROM captcha').all()
+      const rows = db.prepare('SELECT id,guild,joinStamp FROM captcha').all()
       rows.forEach(row => {
-        let guild = client.guilds.get(row.guild)
+        const guild = client.guilds.get(row.guild)
         guild.members.fetch(row.id).then(member => {
           // cache[`${member.guild.id}_${member.id}`] = setTimeout(member.kick, (30 * 60 * 1000) - (Date.now() - row.joinStamp))
         })
@@ -17,12 +17,23 @@ module.exports = {
     },
     message (client, db, moduleName, msg) {
       if (!msg.guild) {
-        let rows = db.prepare('SELECT guild,captcha,attempts FROM captcha WHERE id = ?').all(msg.author.id)
+        const rows = db.prepare('SELECT guild,captcha,attempts FROM captcha WHERE id = ?').all(msg.author.id)
         if (rows.length > 0) {
           rows.forEach(row => {
-            if (row.captcha === msg.content) {
-              let guild = client.guilds.get(row.guild)
-              let role = guild.roles.find(r => r.name === 'Members')
+            if (msg.content.toLowerCase() === 'reroll') {
+              const guild = client.guilds.get(row.guild)
+              guild.members.fetch(msg.author.id).then(member => {
+                const cimg = generate()
+                const { plaintext, buffer } = cimg
+
+                db.prepare('UPDATE captcha SET captcha = ? AND attempts = 5 WHERE id=? AND guild =?').run(plaintext, member.id, member.guild.id)
+                member.send({
+                  files: [buffer]
+                })
+              })
+            } else if (row.captcha === msg.content) {
+              const guild = client.guilds.get(row.guild)
+              const role = guild.roles.find(r => r.name === 'Members')
               guild.members.fetch(msg.author.id).then(member => {
                 clearTimeout(cache[`${guild.id}_${msg.author.id}`])
                 db.prepare('DELETE FROM captcha WHERE id = ? AND guild = ?').run(msg.author.id, row.guild)
@@ -31,7 +42,7 @@ module.exports = {
             } else {
               if (row.attempts === 1) {
                 db.prepare('DELETE FROM captcha WHERE id = ? AND guild = ?').all(msg.author.id, row.guild)
-                let guild = client.guilds.get(row.guild)
+                const guild = client.guilds.get(row.guild)
 
                 msg.author.send('Too many failed attempts.')
                 guild.members.fetch(msg.author.id).then(member => member.kick())
@@ -50,14 +61,14 @@ module.exports = {
       const cimg = generate()
       const { plaintext, buffer } = cimg
 
-      let rows = db.prepare('SELECT guild,captcha FROM captcha WHERE id = ? AND guild = ?').get(member.id, member.guild.id)
+      const rows = db.prepare('SELECT guild,captcha FROM captcha WHERE id = ? AND guild = ?').get(member.id, member.guild.id)
       if (!rows) {
         db.prepare('INSERT INTO captcha (id,guild,captcha,attempts,joinStamp) VALUES (?,?,?,5,?)').run(member.id, member.guild.id, plaintext, member.joinedTimestamp)
 
         member.send({
           embed: {
             title: `Welcome to ${member.guild.name}!`,
-            description: 'Please complete the captcha below to gain access to the server.\n**NOTE:** This is **Case Sensitive**.',
+            description: 'Please complete the captcha below to gain access to the server.\nYou can request a different captcha by typing \'reroll\' here.\n**NOTE:** This is **Case Sensitive**.\nIn case of having issues, ask for support on #verification.',
             fields: [{
               name: '**Why?**',
               value: 'This is to protect the server against malicious raids using automated bots.'
