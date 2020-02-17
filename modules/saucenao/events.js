ddd184321cd99bbcdb43ea9b0214bed82bf35b05
jorgev259 <jorgev259@outlook.es>
2020-02-17 11:19:56 -0600

    limit message sending

--- modules/saucenao/events.js
+++ modules/saucenao/events.js
@@ -8,15 +8,17 @@ module.exports = {
         msg.attachments.forEach(attach => {
           mySauce(attach.url).then(response => {
             const results = response.json.results.filter(e => parseFloat(e.header.similarity) > 80).sort((a, b) => a - b)
-            msg.channel.send(`Found source: ${
-                results.map(e => {
-                    if (e.data.pixiv_id) {
-                        return `<https://www.pixiv.net/en/artworks/${e.data.pixiv_id}>`
-                    } else {
-                        return e.data.ext_urls.map(url => `<${url}>`).join(' - ')
-                    }
-                }).join(' - ')
-            }`)
+            if (results.length) {
+              msg.channel.send(`Found source: ${
+                    results.map(e => {
+                        if (e.data.pixiv_id) {
+                            return `<https://www.pixiv.net/en/artworks/${e.data.pixiv_id}>`
+                        } else {
+                            return e.data.ext_urls.map(url => `<${url}>`).join(' - ')
+                        }
+                    }).join(' - ')
+                }`)
+            }
           }, (error) => {
             console.error('Request encountered an error')
             console.dir(error.request)
