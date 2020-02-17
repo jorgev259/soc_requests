9eebbfa9333d31e52649273119822a33b4dfbfa0
jorgev259 <jorgev259@outlook.es>
2020-02-17 11:11:25 -0600

    saucenao v1

--- modules/saucenao/events.js
+++ modules/saucenao/events.js
@@ -8,7 +8,15 @@ module.exports = {
         msg.attachments.forEach(attach => {
           mySauce(attach.url).then(response => {
             const results = response.json.results.filter(e => parseFloat(e.header.similarity) > 80).sort((a, b) => a - b)
-            console.log(results)
+            msg.channel.send(`Found source: ${
+                results.map(e => {
+                    if (e.data.pixiv_id) {
+                        return `<https://www.pixiv.net/en/artworks/${e.data.pixiv_id}>`
+                    } else {
+                        return e.data.ext_urls.map(url => `<${url}>`).join(' - ')
+                    }
+                }).join(' - ')
+            }`)
           }, (error) => {
             console.error('Request encountered an error')
             console.dir(error.request)
