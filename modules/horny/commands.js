1b174c85907e5763e4ed6ed0aeb4735a267bf1c2
jorgev259 <jorgev259@outlook.es>
2020-04-02 22:28:41 -0600

    add horny

---
+++ modules/horny/commands.js
@@ -0,0 +1,19 @@
+const images = ['tenor.gif', 'bonk.gif']
+module.exports.commands = {
+  horny: {
+    desc: 'Neutralizes a horny.',
+    usage: 'horny [message ID]',
+    async execute (client, msg, param, db) {
+      if (!param[1]) return msg.channel.send('Specify the violating message. >horny [ID]')
+      const viol = await msg.channel.messages.fetch(param[1])
+      viol.member.roles.add('695486787099033620')
+      msg.channel.send({
+        files: [{
+          attachment: images[Math.floor((Math.random() * images.length))],
+          name: 'horny.gif'
+        }]
+      })
+      viol.delete()
+    }
+  }
+}
