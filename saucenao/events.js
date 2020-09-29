fdf8401f29cf64e58c31e51e2e46f3de5c9acbc4
jorgev259 <jorgev259@outlook.es>
2020-09-29 13:32:33 -0600

    tidy up code

--- saucenao/events.js
+++ saucenao/events.js
@@ -10,51 +10,28 @@ module.exports = {
 
     const item = await sequelize.models.saucenao.findOne({ where: { guild: msg.guild.id, channel: msg.channel.id } })
     if (!item || msg.author.bot) return
-    if (msg.attachments.size > 0) checkAttachment(msg)
+    if (msg.attachments.size > 0) msg.attachments.forEach(attach => handleFetch(attach.url, msg))
 
     const urls = getUrls(msg.content)
-    if (urls.size > 0) checkUrls(msg, urls)
+    if (urls.size > 0) for (const url of urls) handleFetch(url, msg)
   }
 }
 
-function checkAttachment (msg) {
-  msg.attachments.forEach(attach => {
-    mySauce(attach.url).then(response => {
-      const results = response.json.results.filter(e => parseFloat(e.header.similarity) > 80).sort((a, b) => a - b)
-      if (results.length) {
-        msg.channel.send(`Found source: ${results.map(e => {
-          if (e.data.pixiv_id) {
-            return `<https://www.pixiv.net/en/artworks/${e.data.pixiv_id}>`
-          } else {
-            return e.data.ext_urls.map(url => `<${url}>`).join(' - ')
-          }
-        }).join(' - ')
-          }`)
-      }
-    }, (error) => {
-      console.error('Request encountered an error')
-      console.dir(error.request)
-    })
+function handleFetch (msg, url) {
+  mySauce(url).then(response => {
+    const results = response.json.results.filter(e => parseFloat(e.header.similarity) > 80).sort((a, b) => a - b)
+    if (results.length) {
+      msg.channel.send(`Found source: ${results.map(e => {
+        if (e.data.pixiv_id) {
+          return `<https://www.pixiv.net/en/artworks/${e.data.pixiv_id}>`
+        } else {
+          return e.data.ext_urls.map(url => `<${url}>`).join(' - ')
+        }
+      }).join(' - ')
+        }`)
+    }
+  }, (error) => {
+    console.error('Request encountered an error')
+    console.dir(error.request)
   })
-}
-
-function checkUrls (msg, urls) {
-  for (const url of urls) {
-    mySauce(url).then(response => {
-      const results = response.json.results.filter(e => parseFloat(e.header.similarity) > 80).sort((a, b) => a - b)
-      if (results.length) {
-        msg.channel.send(`Found source: ${results.map(e => {
-          if (e.data.pixiv_id) {
-            return `<https://www.pixiv.net/en/artworks/${e.data.pixiv_id}>`
-          } else {
-            return e.data.ext_urls.map(url => `<${url}>`).join(' - ')
-          }
-        }).join(' - ')
-          }`)
-      }
-    }, (error) => {
-      console.error('Request encountered an error')
-      console.dir(error.request)
-    })
-  }
 }
