1b1c66a59d5b2ae7c1a8c5a81ef074940859c091
jorgev259 <jorgev259@outlook.es>
2020-09-29 12:17:13 -0600

    update to use sequelize

--- saucenao/commands.js
+++ saucenao/commands.js
@@ -2,16 +2,16 @@ module.exports = {
   enablesauce: {
     desc: 'Enables saucebot on the current channel',
     help: '>enablesauce',
-    async execute (client, msg, param, db) {
-      db.prepare('INSERT OR IGNORE INTO saucenao (guild,channel) VALUES (?,?)').run(msg.guild.id, msg.channel.id)
+    async execute (client, msg, param, sequelize) {
+      await sequelize.models.saucenao.findOrCreate({ where: { guild: msg.guild.id, channel: msg.channel.id } })
       msg.channel.send('Saucenao enabled on this channel')
     }
   },
   disablesauce: {
     desc: 'Disables saucebot on the current channel',
     help: '>disablesauce',
-    async execute (client, msg, param, db) {
-      db.prepare('DELETE FROM saucenao WHERE guild = ? AND channel = ?').run(msg.guild.id, msg.channel.id)
+    async execute (client, msg, param, sequelize) {
+      await sequelize.models.saucenao.destroy({ where: { guild: msg.guild.id, channel: msg.channel.id } })
       msg.channel.send('Saucenao disabled from this channel')
     }
   }
--- saucenao/events.js
+++ saucenao/events.js
@@ -3,25 +3,24 @@ const SauceNAO = require(path.join(process.cwd(), 'node_modules', 'import-cwd'))
 let mySauce
 
 module.exports = {
-  message (client, db, moduleName, msg) {
+  message (client, sequelize, moduleName, msg) {
     const { saucenaoToken } = client.config.saucenao.config
     if (!mySauce) mySauce = new SauceNAO(saucenaoToken)
 
-    const item = db.prepare('SELECT channel FROM saucenao WHERE guild=? AND channel=?').get(msg.guild.id, msg.channel.id)
+    const item = sequelize.models.saucenao.findOne({ where: { guild: msg.guild.id, channel: msg.channel.id } })
     if (item && msg.attachments.size > 0) {
       msg.attachments.forEach(attach => {
         mySauce(attach.url).then(response => {
           const results = response.json.results.filter(e => parseFloat(e.header.similarity) > 80).sort((a, b) => a - b)
           if (results.length) {
-            msg.channel.send(`Found source: ${
-                    results.map(e => {
-                        if (e.data.pixiv_id) {
-                            return `<https://www.pixiv.net/en/artworks/${e.data.pixiv_id}>`
-                        } else {
-                            return e.data.ext_urls.map(url => `<${url}>`).join(' - ')
-                        }
-                    }).join(' - ')
-                }`)
+            msg.channel.send(`Found source: ${results.map(e => {
+              if (e.data.pixiv_id) {
+                return `<https://www.pixiv.net/en/artworks/${e.data.pixiv_id}>`
+              } else {
+                return e.data.ext_urls.map(url => `<${url}>`).join(' - ')
+              }
+            }).join(' - ')
+              }`)
           }
         }, (error) => {
           console.error('Request encountered an error')
